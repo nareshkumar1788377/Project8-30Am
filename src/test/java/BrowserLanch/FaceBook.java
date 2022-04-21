84efa105735427ac4a14468da9a52113c9f018f7
nareshkumar1788377 <nareshkumar1788377@gmail.com>
2022-04-21 19:46:25 +0530

    Naveen's code

--- src/test/java/BrowserLanch/FaceBook.java
+++ src/test/java/BrowserLanch/FaceBook.java
@@ -1,6 +1,8 @@
 package BrowserLanch;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -14,6 +16,12 @@ public class FaceBook {
 		WebDriver driver=new ChromeDriver();
 		
 		driver.get("https://www.flipkart.com/");
+		
+		WebElement email = driver.findElement(By.xpath("(//input[@type='text'])[2]"));
+		email.sendKeys("nareshkumar1788377@gmail.com");
+		
+		WebElement pass = driver.findElement(By.xpath("//input[@type='password']"));
+		pass.sendKeys("12345678");
 		
 	}
 
